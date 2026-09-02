dcdd823452ea86077ead2f9ad9c2eadde62fa759
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-09-02 04:35:34 +0300

    Add onboarding screen and update welcome message with navigation link

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,14 @@
 import { Text, View, StyleSheet } from "react-native";
+import {Link} from "expo-router";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-4xl font-bold text-blue-500">Welcome to the Movies App</Text>
+      <Text className="text-4xl font-bold text-primary">
+          Welcome to the Movies App
+      </Text>
+        <Link href="/onboarding" >On boarding </Link>
+
     </View>
   );
 }
