c5926fea5ed0dfe54c53df8456390e12d9c59fcc
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-09-03 02:20:01 +0300

    Refactor import statements and clean up formatting in index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,14 @@
-import { Text, View, StyleSheet } from "react-native";
-import {Link} from "expo-router";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-4xl font-bold text-primary">
-          Welcome to the Movies App
+        Welcome to the Movies App
       </Text>
-        <Link href="/onboarding" >On boarding </Link>
-
+      <Link href="/onboarding">On boarding </Link>
+      <Link href="/movie/avengers">Avengers </Link>
     </View>
   );
 }
