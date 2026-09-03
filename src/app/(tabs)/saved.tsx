5cbf8d2d41c6b8809350da8cfce2876e46aa33ae
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-09-03 03:09:38 +0300

    Remove unused assets and add tab navigation layout with screens for Home, Search, Saved, and Profile

---
+++ src/app/(tabs)/saved.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Saved = () => {
+  return (
+    <View>
+      <Text>saved</Text>
+    </View>
+  );
+};
+
+export default Saved;
+
+const styles = StyleSheet.create({});
