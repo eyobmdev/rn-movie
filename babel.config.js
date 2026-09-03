d6801d734a88916cd9507020e6b4560e73b3920a
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-09-03 15:09:01 +0300

    Add new icons and images, implement module resolver, and update TypeScript configurations

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,33 @@
 module.exports = function (api) {
-    api.cache(true);
-    return {
-        presets: [
-            ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-            "nativewind/babel",
-        ],
-    };
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@/assets": "./assets",
+            "@": "./src",
+          },
+          extensions: [
+            ".ios.js",
+            ".android.js",
+            ".js",
+            ".jsx",
+            ".json",
+            ".ts",
+            ".tsx",
+            ".png",
+            ".jpg",
+            ".jpeg",
+          ],
+        },
+      ],
+    ],
+  };
 };
--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,11 +1,28 @@
 import { Tabs } from "expo-router";
+import { Image } from "react-native";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
 export default function TabsLayout() {
   return (
     <Tabs>
       <Tabs.Screen
         name="index"
-        options={{ title: "Home", headerShown: false }}
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={icons.home}
+              style={{
+                width: 24,
+                height: 24,
+                tintColor: focused ? "#FFD700" : "#A8A8A8", // example
+              }}
+              resizeMode="contain"
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="search"
