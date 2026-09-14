8030cf7797ef0895c30d8631e9f5bd8d999d6ca1
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-09-14 12:00:47 +0300

    Fix formatting and add missing export for getTrendingMovie function

--- src/services/appwrite.ts
+++ src/services/appwrite.ts
@@ -13,6 +13,7 @@ type Movie = {
   title: string;
   poster_path: string | null;
 };
+
 export const updateSearchCount = async (query: string, movie: Movie) => {
   try {
     const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
@@ -43,5 +44,20 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
   } catch (error) {
     console.log("Error", error);
     throw error;
+  }
+};
+
+export const getTrendingMovie = async (): Promise<
+  TrendingMovie[] | undefined
+> => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+
+    return result.documents as unknown as TrendingMovie[];
+  } catch (error) {
+    console.log(error);
   }
 };
