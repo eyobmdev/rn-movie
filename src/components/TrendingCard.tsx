0468d4b7f4af0ebe9cf2af5f1fc7053d9408709e
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-09-17 15:07:18 +0300

    Refactor TrendingCard component to include movie title and improve structure

--- src/components/TrendingCard.tsx
+++ src/components/TrendingCard.tsx
@@ -1,17 +1,45 @@
+import { images } from "@/constants/images";
+import MaskedView from "@react-native-masked-view/masked-view";
 import { Link } from "expo-router";
-import { Image, TouchableOpacity } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
-const TrendingCard = ({ movie: { movie_id, poster_url }, index }: TrendingCardProps) => {
+const TrendingCard = ({
+  movie: { movie_id, poster_url, title },
+  index,
+}: TrendingCardProps) => {
   return (
-    <Link href={`/movies/${movie_id}`} asChild>
-      <TouchableOpacity className="w-32 relative pl-5">
-        <Image
-          source={{ uri: poster_url }}
-          className="w-32 h-48 rounded-lg"
-          resizeMode="cover"
-        />
-      </TouchableOpacity>
-    </Link>
+    <View className="w-32 relative pl-5">
+      <Link href={`/movies/${movie_id}`} asChild>
+        <TouchableOpacity>
+          <Image
+            source={{ uri: poster_url }}
+            className="w-32 h-48 rounded-lg"
+            resizeMode="cover"
+          />
+          <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
+            <MaskedView
+              maskElement={
+                <Text className="font-bold text-white text-6xl">
+                  {index + 1}
+                </Text>
+              }
+            >
+              <Image
+                source={images.rankingGradient}
+                className="size-14"
+                resizeMode="cover"
+              />
+            </MaskedView>
+          </View>
+          <Text
+            className="text-sm font-bold text-light-200 mt-2"
+            numberOfLines={2}
+          >
+            {title}
+          </Text>
+        </TouchableOpacity>
+      </Link>
+    </View>
   );
 };
 
