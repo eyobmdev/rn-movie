7fb42189bb05b7ae5a1c565b8e1168010d75e1fc
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-09-19 01:24:19 +0300

    Refactor Profile and Saved components to enhance layout and display user icons

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,9 +1,17 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { Image, Text, View } from "react-native";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex-col justify-center gap-5 items-center flex-1">
+        <Image
+          source={icons.person}
+          className="size-10"
+          style={{ tintColor: "#fff" }}
+        />
+        <Text className="text-gray-500 text-base">Person</Text>
+      </View>
     </View>
   );
 };
--- src/app/(tabs)/saved.tsx
+++ src/app/(tabs)/saved.tsx
@@ -1,9 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const Saved = () => {
   return (
-    <View>
-      <Text>saved</Text>
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex-col justify-center gap-5 items-center flex-1">
+        <Image
+          source={icons.save}
+          className="size-10"
+          style={{ tintColor: "#fff" }}
+        />
+        <Text className="text-gray-500 text-base">Save</Text>
+      </View>
     </View>
   );
 };
