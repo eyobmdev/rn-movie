d97b4b8b43c26ea9802bb5c7a136ede094c9a7f2
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-09-18 23:49:01 +0300

    Refactor MovieDetail component to enhance layout and display movie poster

--- src/app/movies/[id].tsx
+++ src/app/movies/[id].tsx
@@ -1,11 +1,24 @@
+import { fetchMovieDetail } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useSearchParams } from "expo-router/build/hooks";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, View } from "react-native";
 
 const MovieDetail = () => {
-  const id = useSearchParams();
+  const id = useSearchParams().get("id");
+  const { data: movie, loading } = useFetch(() => fetchMovieDetail(id ?? ""));
   return (
-    <View className="mt-20">
-      <Text className="text-black">MovieDetail: {id}</Text>
+    <View className=" bg-primary flex-1">
+      <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
+        <View>
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
+            }}
+            className="w-full h-[550px]"
+            resizeMode="stretch"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
